ebb2be9a3f3b7e809807360bdb682715c6a516c0
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-05-08 22:39:40 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
 void findMinMax(int* array, int size, int* max, int* min) {
-    *max = *min = array[0]; // Initialize max and min with the first element of the array
-    for (int i = 1; i < size; i++) {
-        if (array[i] > *max) {
-            *max = array[i]; // Update max if a larger value is found
-        }
-        if (array[i] < *min) {
-            *min = array[i]; // Update min if a smaller value is found
-        }
-    }
+   
 }
 
 int main() {
