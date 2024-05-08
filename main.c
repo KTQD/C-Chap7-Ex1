f6408ea1cd00de0722b9c4040e00337e9c01e638
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-05-08 22:38:35 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
+void findMinMax(int* array, int size, int* max, int* min) {
+    *max = *min = array[0]; // Initialize max and min with the first element of the array
+    for (int i = 1; i < size; i++) {
+        if (array[i] > *max) {
+            *max = array[i]; // Update max if a larger value is found
+        }
+        if (array[i] < *min) {
+            *min = array[i]; // Update min if a smaller value is found
+        }
+    }
+}
+
 int main() {
-    int length, width, area;
+    int data[] = {53, 32, 19, 79, 25, 5, 47};
+    int size = sizeof(data) / sizeof(data[0]);
+    int max, min;
 
-    // Đọc chiều dài và chiều rộng từ người dùng
-    printf("Enter length and width: ");
-    scanf("%d %d", &length, &width);
-
-    // Tính toán diện tích
-    area = length * width;
-
-    // In ra diện tích
-    printf("The area of the rectangle is: %d\n", area);
-
+    findMinMax(data, size, &max, &min);
+    printf("Maximum value: %d\n", max);
+    printf("Minimum value: %d\n", min);
     return 0;
 }
